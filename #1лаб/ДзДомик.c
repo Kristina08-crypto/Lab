f3445187c9e221b08bc6f3a9e69e4976c05cdb4c
Кристина Порошкова <kristina2008kris@mail.ru>
2026-09-15 00:30:15 +0300

    Add files via upload

---
+++ #1лаб/ДзДомик.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <locale.h>
+
+void main() 
+{
+    setlocale(LC_ALL, "Russian");
+    puts("          /\\   ");
+    puts("         /  \\     ");
+    puts("        /    \\     ");
+    puts("       /      \\    ");
+    puts("      /  ____  \\    ");
+    puts("     /  |    |  \\   ");
+    puts("    /___|____|___\\  ");
+    puts("    |            |   ");
+    puts("    |   __  __   |  ");
+    puts("    |  |  ||  |  |  ");
+    puts("    |__|__||__|__|  ");
+    getchar();
+}
