2600f2864a6ab55ddda21dbb31b0155540dd9209
Кристина Порошкова <kristina2008kris@mail.ru>
2026-09-23 23:13:45 +0300

    Add files via upload

---
+++ №2лаб/2задание.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+#include <locale.h>
+int main()
+{
+    int N = 16, K = 15;
+    setlocale(LC_ALL, "Russian");
+    printf("Сейчас %d часов %d минут 00 секунд\n", N, K);
+    printf("Идёт %d минута суток\n", N * 60 + K + 1);
+    printf("До полуночи осталось %d часов и %d минут \n", 23 - N, 60 - K);
+    printf("С 8.00 прошло %d секунд\n", ((N - 8) * 3600) + (K * 60));
+    printf("Текущий час= %.2f суток и текущая минута = %.2f часа\n", N / 24., K / 60.);
+}
